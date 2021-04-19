90ea32351d8d026ef703f37d758435ac235c6ba2
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2021-04-19 14:40:32 -0300

    Html/Css e Js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Carne - 400gramas por pessoa + de 6 horas - 650 gramas
+//Cerveja - 1200ml por pessoa + 6 horas - 2000ml
+// Refrigerante/agua - 1000ml por pessoa + 6 horas 1500 ml
+
+//crianças valem por 0,5
+
+
